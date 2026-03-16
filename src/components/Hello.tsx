f045becffaec0de1625a4bd2a0427b73aa5ef5b2
Azure Pipeline <pipeline@azure-devops.local>
2026-03-16 01:45:19 +0000

    feat: initial project structure from react template
    Generated by Azure Pipeline
    - Template  : react
    - Project   : test-react4
    - Repo      : test-react4

---
+++ src/components/Hello.tsx
@@ -0,0 +1,5 @@
+const Hello: React.FC = () => {
+  return <p>Hello World from React + TS</p>;
+};
+
+export default Hello;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import Hello from "../components/Hello";
+
+const Home: React.FC = () => {
+  return (
+    <main>
+      <Hello />
+    </main>
+  );
+};
+
+export default Home;
